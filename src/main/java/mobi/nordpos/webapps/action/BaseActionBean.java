123d17d5456e9de7c505323c54e4773c4d92aa92
svininykh <svininykh@gmail.com>
2014-12-11 12:58:22 +0600

    Major: Migrate to Maven.

---
+++ src/main/java/mobi/nordpos/webapps/action/BaseActionBean.java
@@ -0,0 +1,93 @@
+/**
+ * Copyright (c) 2012-2014 Nord Trading Network.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package mobi.nordpos.webapps.action;
+
+import java.util.HashMap;
+import java.util.Map;
+import javax.servlet.http.HttpServletRequest;
+import mobi.nordpos.webapps.ext.MobileActionBeanContext;
+import mobi.nordpos.webapps.ext.MyLocalePicker;
+import net.sourceforge.stripes.action.ActionBean;
+import net.sourceforge.stripes.action.ActionBeanContext;
+import net.sourceforge.stripes.controller.StripesFilter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Andrey Svininykh <svininykh@gmail.com>
+ */
+public abstract class BaseActionBean implements ActionBean {
+
+    private MobileActionBeanContext context;
+
+    Logger logger = LoggerFactory.getLogger(this.getClass().getName());
+
+
+    @Override
+    public MobileActionBeanContext getContext() {
+        return this.context;
+    }
+
+    @Override
+    public void setContext(ActionBeanContext actionBeanContext) {
+        this.context = (MobileActionBeanContext) actionBeanContext;
+    }
+
+    @SuppressWarnings("unchecked")
+    public String getLastUrl() {
+        HttpServletRequest req = getContext().getRequest();
+        StringBuilder sb = new StringBuilder();
+
+        // Start with the URI and the path
+        String uri = (String) req.getAttribute("javax.servlet.forward.request_uri");
+        String path = (String) req.getAttribute("javax.servlet.forward.path_info");
+        if (uri == null) {
+            uri = req.getRequestURI();
+            path = req.getPathInfo();
+        }
+        sb.append(uri);
+        if (path != null) {
+            sb.append(path);
+        }
+
+        // Now the request parameters
+        sb.append('?');
+        Map<String, String[]> map
+                = new HashMap<String, String[]>(req.getParameterMap());
+
+        // Remove previous locale parameter, if present.
+        map.remove(MyLocalePicker.LOCALE);
+
+        // Append the parameters to the URL
+        for (String key : map.keySet()) {
+            String[] values = map.get(key);
+            for (String value : values) {
+                sb.append(key).append('=').append(value).append('&');
+            }
+        }
+        // Remove the last '&'
+        sb.deleteCharAt(sb.length() - 1);
+
+        return sb.toString();
+    }
+
+    public String getLocalizationKey(String key) {
+        return StripesFilter.getConfiguration().getLocalizationBundleFactory()
+                .getFormFieldBundle(getContext().getLocale()).getString(key);
+    }
+
+
+}
